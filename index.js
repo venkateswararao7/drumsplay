cb9be7b744896bccf9d203bea506c662cfb1262d
venkateswararao7 <85663469+venkateswararao7@users.noreply.github.com>
2022-11-20 20:05:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,29 +17,29 @@ document.addEventListener("keypress",function(event){
 function makeSound(letter)
 {
     if(letter=='w'){
-        var  audio =new Audio("sounds/tom-1.mp3");
+        var  audio =new Audio("tom-1.mp3");
        
     }
     else if(letter=='a'){
-        var audio=new Audio("sounds/tom-2.mp3");
+        var audio=new Audio("tom-2.mp3");
         
     }
     else if(letter=='s'){
-        var audio=new Audio("sounds/tom-3.mp3");
+        var audio=new Audio("tom-3.mp3");
         
     }
     else if(letter=='d'){
-        var audio =new Audio("sounds/tom-4.mp3");
+        var audio =new Audio("tom-4.mp3");
         
     }
     else if(letter=='j'){
-        var audio=new Audio("sounds/snare.mp3");
+        var audio=new Audio("snare.mp3");
     }
     else if(letter=='k'){
-        var audio=new Audio("sounds/crash.mp3");
+        var audio=new Audio("crash.mp3");
     }
     else{
-        var audio =new Audio("sounds/kick-bass.mp3");
+        var audio =new Audio("kick-bass.mp3");
     }
     audio.play();
 
